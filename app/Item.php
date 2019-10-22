3452efb03c59ea1081adf5ba93895603749a4b5c
mjnayem <mjnayem@gmail.com>
2019-10-22 21:48:56 +0600

    all api created

--- app/Item.php
+++ app/Item.php
@@ -6,5 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+
+    protected $fillable = [
+        'name',
+        'rating',
+        'category',
+        'image',
+        'reputation',
+        'reputationBadge',
+        'price',
+        'availability',
+
+    ];
+
+    protected $hidden =[
+        'id',
+        'hotelier_id',
+        'location_id',
+        'updated_at',
+        'created_at'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Location');
+    }
+
+    public function location(){
+        return $this->belongsTo('App\Location', 'location_id', 'id');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,10 @@ Route::group(['prefix' => 'v1', 'middleware' => 'cors'], function () {
         'uses' => '\App\Http\Controllers\Auth\LoginController@login'
     ]);
 
+    Route::get('bookItem/{itemId}', [
+        'as' => 'bookItem',
+        'uses' => 'ItemController@bookItem'
+    ]);
 
 
     Route::group(['middleware' => 'auth:api'], function () {
@@ -37,11 +41,33 @@ Route::group(['prefix' => 'v1', 'middleware' => 'cors'], function () {
 
 
 
+
+
         Route::group(['prefix' => 'user'], function () {
 
             Route::post('addItem', [
                 'as' => 'addItem',
                 'uses' => 'ItemController@addItemUser'
+            ]);
+
+            Route::post('updateItem/{itemId}', [
+                'as' => 'updateItem',
+                'uses' => 'ItemController@updateItem'
+            ]);
+
+            Route::get('deleteItem/{itemId}', [
+                'as' => 'deleteItem',
+                'uses' => 'ItemController@deleteItem'
+            ]);
+
+            Route::get('getSingleItem/{itemId}', [
+                'as' => 'getSingleItem',
+                'uses' => 'ItemController@getSingleItem'
+            ]);
+
+            Route::get('getItemByUser/{userId}', [
+                'as' => 'getItemByUser',
+                'uses' => 'ItemController@getItemByUser'
             ]);
         });
     });
